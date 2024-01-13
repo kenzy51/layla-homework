c809b05558556112980e33e544d9b74e09219056
Kenzy2112 <123618123+kenzy51@users.noreply.github.com>
2024-01-13 17:01:33 +0600

    added javascript

---
+++ index.js
@@ -0,0 +1,48 @@
+const object = {
+  name: "layla",
+  age: 17,
+};
+
+// array can contain such
+const array = ["asdasd", "asd"];
+const arrayNumbers = [21, 213, 42];
+const arrayObjects = [
+  {
+    name: "Layla",
+    age: 20,
+  },
+  {
+    name: "Layla",
+    age: 20,
+  },
+  {
+    name: "Layla",
+    age: 20,
+  },
+];
+
+// console.log(typeof object);
+
+// 2 типа
+// примитивы - number,string, null,undefined
+// ссылочные-object,array, funcion(exlude)
+
+// functions
+const function1 = () => {
+  console.log("hi world");
+};
+
+function function2() {
+  console.log("hi world 231");
+}
+
+//
+
+let work = null
+
+console.log(work);
+
+
+// 
+// homework
+// object can be created with 3 methods()
